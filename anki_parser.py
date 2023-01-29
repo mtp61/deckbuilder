6b3fdd50c3300c450ebcd40e1961fa66ab72f478
mtp61 <mpauly@college.harvard.edu>
2023-01-29 13:58:00 -0500

    Fix parser output location bug

--- anki_parser.py
+++ anki_parser.py
@@ -15,17 +15,19 @@ def get_args():
             description='Converts a single file of a specified format (see anki_file_format.md) into an anki deck')
     parser.add_argument(
             '-d', '--deck_name',
-            type=os.path.abspath,
+            type=str,
+            required=True,
             help='name of the created deck')
     parser.add_argument(
             '-n', '--notes_file',
             type=os.path.abspath,
+            required=True,
             help='path to the notes file')
     parser.add_argument(
             '-o', '--output_dir',
             type=os.path.abspath,
-            help='path to the output directory (default: ~/Downloads)',
-            default=os.path.expanduser('~/Downloads'))
+            default=os.path.expanduser('~/Downloads'),
+            help='path to the output directory (default: ~/Downloads)')
     parser.add_argument(
             '-i', '--image_path',
             type=os.path.abspath,
